815f718d88c2bac04a26cdce9b21807e73430212
sheezanmk <sheezankhan09@hotmail.com>
2026-02-26 16:19:15 +1100

    Adds initial Spring Boot set up with MySQL config and swagger Set up

---
+++ src/main/java/com/sheezanmk/employee_creator_api/HealthController.java
@@ -0,0 +1,13 @@
+package com.sheezanmk.employee_creator_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
---
+++ src/test/java/com/sheezanmk/employee_creator_api/EmployeeCreatorApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sheezanmk.employee_creator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EmployeeCreatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
